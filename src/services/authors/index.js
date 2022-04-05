19bc3d641afa01805d374eb29536e916d85b136d
AZIZ <a.matyaqubov1471@gmail.com>
2022-04-05 23:40:19 +0200

    create post

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -10,25 +10,33 @@ const authorsRouter = express.Router()
 
 const currentFilePath = fileURLToPath(import.meta.url)
 const parentFolderPath = dirname(currentFilePath)
-const usersJSONPath = join(parentFolderPath, "db.json")
+const authorsJSONPath = join(parentFolderPath, "db.json")
 
 
 authorsRouter.get('/', (req, res)=> {
-    const fileContent = fs.readFileSync(usersJSONPath)
+    const fileContent = fs.readFileSync(authorsJSONPath)
     const authorsArray = JSON.parse(fileContent)
-    // res.send(authorsArray)
+    
     res.send(authorsArray)
 })
 
 authorsRouter.get('/:id', (req, res)=>{
 
      const authorId = req.params.authorId
-     const authorsArray = JSON.parse(fs.readFileSync(usersJSONPath))
+     const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath))
 
     const foundAuthor = authorsArray.find(author => author.id === authorId)
     res.send(foundAuthor)
 })
 
+
+authorsRouter.post('/', (req, res) => {
+    const newAuthor = { ...req.body, id: uniqid(), avatar: 'https://ui-avatars.com/api/?name=John+Doe' }
+    const authorsArray = JSON.parse(fs.readFileSync(authorsJSONPath))
+    authorsArray.push(newAuthor)
+    fs.writeFileSync(authorsJSONPath, JSON.stringify(authorsArray))
+    res.status(201).send({ id: newAuthor.id })
+})
 
 
 
